80656eb0d0342619e90def1308de83dedd728d16
Justin <meimar@ualberta.ca>
2024-09-30 20:24:57 -0600

    Update config tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,9 +5,13 @@ def test_valid_config(sample_valid_config: Config):
     conf = sample_valid_config 
     assert conf is not None
     assert conf.test_dir is not None
-    assert conf.sub_packages is not None
-    for sp in conf.sub_packages:
-        assert sp.tests is not None
+    assert conf.packages is not None
+    for pkg in conf.packages:
+        assert pkg.subpackages is not None
+        for spkg in pkg.subpackages:
+            assert spkg is not None
+            assert len(spkg.tests) > 0
+
     assert sample_valid_config.error_collection == False
     assert os.path.exists(sample_valid_config.test_dir)
 
